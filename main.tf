a1cf2432e8ee136fcd58a4fd344895aedeee55d4
agusjuli <agusjuli@yahoo.com.sg>
2025-04-19 17:55:08 +0800

    Fourth commit

--- main.tf
+++ main.tf
@@ -20,8 +20,11 @@ module "sqs" {
 }
 
 module "ecs" {
-  source    = "./modules/ecs"
-  ecr_repos = module.ecr.repo_urls
-  s3_bucket = module.s3.bucket_name
-  sqs_url   = module.sqs.queue_url
+  source = "./modules/ecs"
+
+  ecr_repos       = module.ecr.repo_urls
+  s3_bucket       = module.s3.bucket_name
+  sqs_url         = module.sqs.queue_url
+  subnet_ids      = var.subnet_ids
+  security_groups = var.security_groups
 }
